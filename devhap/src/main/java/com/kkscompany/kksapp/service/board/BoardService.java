b3524ed623f8b096328d9b9f2674826163bb59b1
dks37 <dks37@192.168.123.139>
2019-05-06 23:13:50 +0900

    마이바티스 연동 resultType map 상속 객체로 바꾸고 put 오버로딩 해주면 key를 커스터마이징 가능함.

--- devhap/src/main/java/com/kkscompany/kksapp/service/board/BoardService.java
+++ devhap/src/main/java/com/kkscompany/kksapp/service/board/BoardService.java
@@ -3,18 +3,23 @@ package com.kkscompany.kksapp.service.board;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kkscompany.kksapp.HomeController;
 import com.kkscompany.kksapp.dao.CommonDao;
 
 @Service
 public class BoardService {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	CommonDao commonDao;
 	
 	public List selectList(Map param) {		
-		return commonDao.selectList("selectList", param); 
+		logger.info("BoardService called successfully");
+		return commonDao.selectList("board.selectList", param); 
 	}
 }
